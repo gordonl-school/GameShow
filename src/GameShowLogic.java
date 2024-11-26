d10ff94666c5a8587b1b6999378ce31edca5433a
BT_1N3_03 <sample@bths.edu>
2024-11-26 14:00:31 -0500

    Implemented initial pauses when needed

--- src/GameShowLogic.java
+++ src/GameShowLogic.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.lang.Thread; //https://www.geeksforgeeks.org/thread-sleep-method-in-java-with-examples/
 
 public class GameShowLogic {
     private Scanner myScanner;
@@ -25,9 +25,18 @@ public class GameShowLogic {
         Player player2 = new Player(secondPlayerName);
         System.out.println("We have our two players! Let me explain the rules.");
         System.out.println("Each player has three lives and we'll have multiple rounds with multiple" + "\n" + "questions.");
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {} // https://stackoverflow.com/questions/43507587/how-to-pause-my-java-program-for-2-seconds (used multiple times)
         System.out.println("Each round has a max amount of points, 200 points, that can be earned." + "\n" + "Once somebody hits 1000 points, the game will end.");
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException e) {}
         System.out.println("Now that the game is explain and we're all ready, let's start!");
         System.out.println("Let's flip a coin to see who goes first.");
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {}
         int result = (int)(Math.random() * 2) + 1;
         System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
                 "⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠠⠤⠤⠤⠶⠶⠤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
@@ -44,8 +53,11 @@ public class GameShowLogic {
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠿⠟⠁⢸⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀\n" +
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠶⠾⠿⠿⢿⣿⣿⣿⣿⣿⣿⡇⠀\n" +
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠉⠉⠉⠉⠉⠉⠁⠀"); //https://emojicombos.com/coinflip-ascii-art
-        // thinking about adding a pause
-        System.out.println("     ______________\n" +
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {}
+        System.out.println();
+        System.out.println("                 ______________\n" +
                 "    __,.,---'''''              '''''---..._\n" +
                 " ,-'             .....:::''::.:            '`-.\n" +
                 "'           ...:::.....       '\n" +
@@ -54,6 +66,9 @@ public class GameShowLogic {
                 " '-.._''`---.....______________.....---''__,,-\n" +
                 "      ''`---.....______________.....---''"); //https://www.asciiart.eu/miscellaneous/money
         System.out.println("...");
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException e) {}
         if (result == 1) {
             player1.changePriority();
             String player1Name = player1.getName();
